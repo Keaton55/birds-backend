a0145510dbe7f17e12b0f13f8ef8f93144acb5dc
Keaton Manwaring <main@Keatons-MacBook-Pro.local>
2024-11-18 20:39:19 -0700

    tesitng
    :

--- server.js
+++ server.js
@@ -34,10 +34,6 @@ const db = knex({
   client: 'pg',
   connection: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
-  pool: {
-    min: 2,
-    max: 10
-}
 });
 
 app.use(bodyParser.json());
